db92f2aa57ede85ef79b19ea939fd5e21f1f74ae
afonsitosDEV <151232068+afonsitosDEV@users.noreply.github.com>
2024-02-23 18:18:43 -0300

    Add files via upload

---
+++ rpg/DragaoGold.cs
@@ -0,0 +1,9 @@
+﻿class DragaoGold : Monstro
+{
+    public DragaoGold() : base("Dragão", 500, 25, 15, 400, 400) { }
+
+    public override Monstro EncontrarMonstro()
+    {
+        return new DragaoGold();
+    }
+}
